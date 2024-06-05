284bcf15ac03b543cb617955728ad9766a95b03f
LeBaudry <sam.bdry14@gmail.com>
2024-06-05 15:04:16 +0200

    Merge remote-tracking branch 'origin/DEV' into DEV
    # Conflicts:
    #	src/config/Routes.php
    #	src/models/ArticleModels.php
    #	src/models/FamilleModels.php

--- src/config/Routes.php
+++ src/config/Routes.php
@@ -14,6 +14,7 @@ use src\controllers\RegisterController;
 use src\controllers\ClientController;
 use src\controllers\StockController;
 use src\controllers\UtilisateurController;
+use src\middlewares\ResponseMiddleware;
 
 class Routes{
 
@@ -47,7 +48,7 @@ class Routes{
             $group->get('/readFournisseurs', FournisseurController::class . ':readFournisseurs');
             $group->post('/createFournisseur', FournisseurController::class . ':createFournisseur');
             $group->put('/updateFournisseur/{id}', FournisseurController::class . ':updateFournisseur');
-        });
+        })->add(new ResponseMiddleware());
     }
 
     public static function familleRoutes($app): void
@@ -56,7 +57,7 @@ class Routes{
             $group->get('/readFamille', FamilleController::class . ':readFamille');
             $group->post('/createFamille', FamilleController::class . ':createFamille');
             $group->put('/updateFamille/{id}', FamilleController::class . ':updateFamille');
-        });
+        })->add(new ResponseMiddleware());
     }
 
     public static function commandesRoutes($app): void
@@ -67,7 +68,7 @@ class Routes{
             $group->post('/createCommandes', CommandeController::class . ':createCommandes');
             $group->put('/deleteCommande/{id}', CommandeController::class . ':deleteCommandes');
             $group->post('/updateCommande/{id}', CommandeController::class . ':updateCommandes');
-        });
+        })->add(new ResponseMiddleware());
     }
 
     public static function clientsRoutes($app): void
@@ -76,7 +77,7 @@ class Routes{
         $app->group('/clients', function (RouteCollectorProxy $group) {
             $group->post('/register', RegisterController::class . ':register');
             $group->get('/readClient', ClientController::class . ':readClient');
-        });
+        })->add(new ResponseMiddleware());
     }
 
     public static function articlesRoutes($app): void
@@ -87,7 +88,7 @@ class Routes{
             $group->get('/articleFamille', ArticleController::class . ':articleFamille');
             $group->post('/createArticle', ArticleController::class . ':createArticle');
             $group->put('/updateArticle/{id}', ArticleController::class . ':updateArticle');
-        });
+        })->add(new ResponseMiddleware());
     }
 
     public static function utilisateursRoutes($app): void
@@ -96,7 +97,7 @@ class Routes{
             $group->get('/readUtilisateurs', UtilisateurController::class . ':readUtilisateurs');
             $group->post('/createUtilisateur', UtilisateurController::class . ':createUtilisateur');
             $group->put('/updateUtilisateur/{id}', AdresseController::class . ':updateUtilisateur');
-        });
+        })->add(new ResponseMiddleware());
     }
 
     public static function adressesRoutes($app): void
@@ -105,7 +106,7 @@ class Routes{
             $group->put('/updateAdresse/{id}', AdresseController::class . ':updateAdresse');
             $group ->post('/createAdresse', AdresseController::class . ':createAdresse');
 
-        });
+        })->add(new ResponseMiddleware());
     }
 
 
